4dbde5bdf394b6513c1b4636bda94ecc4889deb7
đào ngọc tùng <39922540+tungdaoUTE@users.noreply.github.com>
2021-01-28 23:50:12 +0700

    đăng nhập hệ thống

---
+++ PhoHuyenOrder/src/connection/ConnectMysql.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+ 
+public class ConnectMysql {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/phohuyen";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "";
+ 
+    /**
+     * main
+     * 
+     * @author viettuts.vn
+     * @param args
+     */
+    public static void main(String args[]) {
+        try {
+            // connnect to database 'testdb'
+            Connection conn = getConnection();
+            // crate statement
+            Statement stmt = conn.createStatement();
+            // get data from table 'student'
+            ResultSet rs = stmt.executeQuery("select * from user");
+            // show data
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) 
+                        + "  " + rs.getString(3));
+            }
+            // close connection
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+//    public static Connection getConnection(String dbURL, String userName, 
+//            String password) {
+//        Connection conn = null;
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            conn = DriverManager.getConnection(dbURL, userName, password);
+//            System.out.println("connect successfully!");
+//        } catch (Exception ex) {
+//            System.out.println("connect failure!");
+//            ex.printStackTrace();
+//        }
+//        return conn;
+//    }
+        public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+}
